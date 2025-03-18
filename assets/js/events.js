acd20f41b5e4f31467d0b9e561c4f425cb5780be
Bo Nicolajsen <>
2025-03-18 09:59:35 +0100

    done

--- assets/js/events.js
+++ assets/js/events.js
@@ -2,26 +2,35 @@
 
 /*opgave 1 plus minus*/
 let myButtonValue = 0;
+//feltet der vises resultat i
+
 const myButtonResult = document.getElementById('buttonResult');
 const myAddButton = document.getElementById('addButton');
 const mySubButton = document.getElementById('subtractButton');
 
 
-myAddButton.addEventListener('click', (e) => {
-    e.preventDefault();
+// skriv din eventhandler kode her ---------------------------------------
+
+
+
+
+//-------------------------------------------------------------------------
+
+// named functions du kan kalde fra eventhandlers
+function subtractValue() {
+ 
     myButtonValue = myButtonValue - 1;
 
     showResult('værdi: ' + myButtonValue, myButtonResult)
-});
+}
 
 
-mySubButton.addEventListener('click', (e) => {
-    e.preventDefault();
-
+function addValue() {
+ 
     myButtonValue = myButtonValue + 1;
 
     showResult('værdi: ' + myButtonValue, myButtonResult)
-});
+}
 
 
 
@@ -33,14 +42,19 @@ const myDiceRes = document.getElementById('diceResult');
 const diceButton = document.getElementById('rollDiceOne');
 
 
-diceButton.addEventListener('click', (e) => {
-    e.preventDefault();
 
+  
+    /* disse to liner skal afvikles i en arrow funktion bundet til en eventhandler på diceButton
     let diceRoll = getRandomNumber(1, 6);
+    showResult(diceRoll, myDiceRes) */
 
-    showResult(diceRoll, myDiceRes)
+// skriv din eventhandler kode her ---------------------------------------
 
-});
+
+
+
+//-------------------------------------------------------------------------
+
 
 
 
@@ -54,21 +68,21 @@ diceButton.addEventListener('click', (e) => {
 const myLiveText = document.getElementById('myLiveText');
 let myLiveTextResult = document.getElementById('tasteResult');
 
-myLiveText.addEventListener('keyup', (e) => {
-    e.preventDefault();
-
-    showResult(myLiveText.value, myLiveTextResult);
+// skriv din eventhandler kode her ---------------------------------------
 
 
-});
 
+
+//-------------------------------------------------------------------------
 
 
 
 
 
 
-/* view code*/
+
+
+/* view code  DO NOT FIDDLE WIT THIS CODE*/
 /* viser data i et DOM element, kræver to parametre: data til at vise og elementet det skal vises i.*/
 function showResult(myData, myElement) {
     myElement.innerHTML = myData;
